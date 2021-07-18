68a75a9f44255592ab7bc8d9c53ac7a2f9836691
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-18 15:13:39 +0300

    add form components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,21 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   appName = 'QuoteApp';
-  quotes:Quote[]=[new Quote(0,0,"fake it until you make it",new Date(2021,7,16),"martin Luther","Morara")]
+  title='QuoteApp';
+  quotes:Quote[]=[new Quote(1,0,0,"fake it until you make it",new Date(2021,7,16),"martin Luther","Morara")]
   constructor(){}
-  
+  toggleQuote(index:any){
+    this.quotes[index].showDetails=!this.quotes[index].showDetails
+    
+  }
   deleteQuote(index:any){
     this.quotes.splice(index,1);
+  }
+  addNewQuote(quote:Quote){
+    const quoteLength=this.quotes.length;
+    quote.id=quoteLength +1;
+    quote.postTime=new Date(quote.postTime);
+    this.quotes.push(quote);
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
