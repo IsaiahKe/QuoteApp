4bfea780ab11032cad285f837c844b9dc4f526cf
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-18 11:09:56 +0300

    add quote model

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,6 @@
 export class Quote {
-    constructor(public upVote:number,public downVote:number,public say:string,public postTime:Date,public author:string,public userName:string){}
+    showDetails:boolean
+    constructor(public upVote:number,public downVote:number,public say:string,public postTime:Date,public author:string,public userName:string){
+        this.showDetails=false;
+    }
 }
