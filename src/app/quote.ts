e24d5266920a5b55675d624d6ef45bce82d27bf4
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-18 15:19:44 +0300

    added model propety

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDetails:boolean
-    constructor(public upVote:number,public downVote:number,public say:string,public postTime:Date,public author:string,public userName:string){
+    constructor(public id:number,public upVote:number,public downVote:number,public say:string,public postTime:Date,public author:string,public userName:string){
         this.showDetails=false;
     }
 }
