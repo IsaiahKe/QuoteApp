14883cbbe3c073aba2c59c9e9a7780c1cfd10732
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-18 11:33:53 +0300

    remove unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+//import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    QuoteDetailsComponent
+    //QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
