3e1603687317babf08684ba5b7b2b2dbe84fb2e3
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-18 11:08:32 +0300

    add app root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
