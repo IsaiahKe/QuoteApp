e5ff78f9d4bd12faebdcc96614c8197b2056c147
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-18 08:50:31 +0300

    make component quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QuoteApp';
+  quotes:Quote[]=[new Quote(3,4,"fake ituntil you make it",new Date(2021,7,16),"martin Luther","Morara")]
 }
