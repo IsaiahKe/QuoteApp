c774cdb5568a5c40849bbebd717e7e4f679406c6
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-19 17:25:55 +0300

    Deployment files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,41 +8,41 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   appName = 'QuoteApp';
-  title='QuoteApp';
-  color='light-blue';
-  quotes:Quote[]=[new Quote(1,0,0,"fake it until you make it",new Date(2021,7,16),"martin Luther","Morara")]
-  constructor(){}
-  toggleQuote(index:any){
-    this.quotes[index].showDetails=!this.quotes[index].showDetails
-    
-  }
-  deleteQuote(index:any){
-    this.quotes.splice(index,1);
-  }
-  addNewQuote(quote:Quote){
+  title = 'QuoteApp';
+  color = 'light-blue';
+  quotes: Quote[] = []
+  constructor() { }
+  toggleQuote(index: any) {
+    this.quotes[index].showDetails = !this.quotes[index].showDetails
 
-    let id=this.quotes.length+1
-    this.quotes.push(
-      new Quote(id,0,0,quote.say,new Date(),quote.author,quote.userName)
-    );
-    quote.author='';
-    quote.say=''
-    quote.userName='';
-    
   }
-  addVote(index:any){
+  deleteQuote(index: any) {
+    this.quotes.splice(index, 1);
+  }
+  addNewQuote(quote: Quote) {
+
+    let id = this.quotes.length + 1
+    this.quotes.push(
+      new Quote(id, 0, 0, quote.say, new Date(), quote.author, quote.userName)
+    );
+    quote.author = '';
+    quote.say = ''
+    quote.userName = '';
+
+  }
+  addVote(index: any) {
     this.quotes[index].upVote++;
   }
-  reduceVote(index:any){
+  reduceVote(index: any) {
     this.quotes[index].downVote++
   }
- getHighest(index:any){
-   Math.max(this.quotes[index].upVote);
-   
- }
+  getHighest(index: any) {
+    Math.max(this.quotes[index].upVote);
+
+  }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    
+
   }
 }
