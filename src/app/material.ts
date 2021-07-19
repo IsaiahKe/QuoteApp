62d4fd6fb7bfda36604f5bef518889bdb6cdc811
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-07-19 16:48:31 +0300

    Add material

---
+++ src/app/material.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+    imports:[
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule
+    ],
+    exports:[
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule
+    ]
+})
+export class Material{}
